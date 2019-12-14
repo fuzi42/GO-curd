fcc03b228a753703779a2d249fd4c3b82c9d921a
fuzi42 <36104616+fuzi42@users.noreply.github.com>
2019-12-14 18:53:01 +0800

    add upload
    add upload

--- index.go
+++ index.go
@@ -4,9 +4,10 @@ package main
 import (
     "fmt"
    "io"
+    "os"
     "log"
-	"net/http"
-	"encoding/json"
+    "net/http"
+    "encoding/json"
     "time"
    "database/sql"
  _ "github.com/go-sql-driver/mysql"
@@ -24,9 +25,10 @@ func add(w http.ResponseWriter, r *http.Request){
 	title  := r.Form.Get("title")        //标题
 	messages := r.Form.Get("messages")   //信息文本
 	date := r.Form.Get("time")			//时间
+	img := r.Form.Get("img")			//图片
 	id := time.Now().Unix()             //id
 	fmt.Println(id,title,messages,date)
-	result, err := db.Exec("insert into work (id,title,messages,date) values (?,?,?,?)",id,title,messages,date)
+	result, err := db.Exec("insert into work (id,title,messages,date,img) values (?,?,?,?,?)",id,title,messages,date,img)
 	if err != nil{
 		fmt.Println(err)
 		return 
@@ -82,10 +84,11 @@ func gai(w http.ResponseWriter, r *http.Request){
 	title  := r.Form.Get("title")        //标题
 	messages := r.Form.Get("messages")   //信息文本
 	date := r.Form.Get("time")			//时间
+	img := r.Form.Get("img")			//图片
 	query := r.URL.Query()
 	id := query["gai"][0]  				//id
 	fmt.Println(id,title,messages,date)
-	result, err := db.Exec("update work set title=?,messages=?,date=? where id=?",title,messages,date,id)
+	result, err := db.Exec("update work set title=?,messages=?,date=?,img=?  where id=?",title,messages,date,img,id)
 	if err != nil{
 		fmt.Println(err)
 		return 
@@ -94,13 +97,34 @@ func gai(w http.ResponseWriter, r *http.Request){
 	fmt.Println("success！")
 	io.WriteString(w, "修改成功！")
 }
+func UploadImage(w http.ResponseWriter, r *http.Request) {
+//     // 接受文件
+	file, header, err := r.FormFile("img")
+    if err != nil {
+			return
+    }
+    fmt.Println(header.Filename+"sadfsdfsd")
+//     log.Printf("selected file name is %s", header.Filename)
+//     // 将文件拷贝到指定路径下，或者其他文件操作
+    dst, err := os.Create("./images/"+header.Filename)
+    if err != nil {
+        // ignore
+    }
+    _, err = io.Copy(dst, file)
+    if err != nil {
+        // ignore
+    }
+  io.WriteString(w, "上传成功！")
 
+}
 func main() {
 	http.HandleFunc("/add", add)              //添加路由
 	http.HandleFunc("/cha", cha)              //查询路由
 	http.HandleFunc("/shan", shan)            //删除路由
 	http.HandleFunc("/gai", gai)              //修改路由
+	http.HandleFunc("/upload", UploadImage)   //上传图片路由
 
+	http.Handle("/", http.FileServer(http.Dir("./images")))
         err := http.ListenAndServe(":8000", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
